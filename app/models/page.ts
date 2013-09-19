c816274783bdb1944cf6bb4fba902ae89bf7a9c9
seikichi <seikichi@kmc.gr.jp>
2013-09-19 18:40:50 +0900

    removes app/collections directory
    Backbone.js の慣習では model と collection のディレクトリを分けるっぽいのだが
    model のインターフェースだけ外に出したいなー変な考えが思いついた際に
    同じモジュール内に model と関連する collection が無いと色々と面倒だと感じたので collections ディレクトリ消してみた

--- app/models/page.ts
+++ app/models/page.ts
@@ -1,19 +1,37 @@
+import $ = require('jquery');
 import Backbone = require('backbone');
 
-class PageModel extends Backbone.Model<PageModel.Attribuets> {
+module Page {
+  // public
+  export interface ModelInterface { }
+  export interface CollectionInterface { }
+  export interface Attributes {
+    name?: string;
+    originalPageNum?: number;
+  }
+  export var createPdfPageCollection: () => CollectionInterface  = () => {
+    return new PdfPageCollection();
+  }
+
+  // private
+  class PdfPageModel extends Backbone.Model<Attributes> implements ModelInterface {
     defaults() {
-        return {
-            name: '',
-            originalPageNum: 0,
-        };
+      return {
+        name: 'no page title',
+        originalPageNum: 0,
+      };
     }
+    // TODO
+    // loadPageImage(): JQueryDeffered<...>;
+  }
+
+  class PdfPageCollection extends Backbone.Collection<PdfPageModel, Attributes>
+    implements CollectionInterface {
+      constructor(models?: Attributes[], options?: any) {
+        this.model = PdfPageModel;
+        super(models, options);
+      }
+  }
 }
 
-module PageModel {
-    export interface Attribuets {
-        name?: string;
-        originalPageNum?: number;
-    }
-}
-
-export = PageModel;
+export = Page;
--- app/models/pdfpage.ts
+++
@@ -1,9 +0,0 @@
-import $ = require('jquery');
-
-import PageModel = require('models/page');
-import ImageModel = require('models/image');
-
-class PdfPageModel extends PageModel {
-    // loadPageImage(): JQueryDeferred<ImageModel> {
-    // }
-}
