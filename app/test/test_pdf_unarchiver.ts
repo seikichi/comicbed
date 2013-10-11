8d7b2e6a45d9e45727865b84b1443e87998ac54e
seikichi <seikichi@kmc.gr.jp>
2013-10-11 14:16:46 +0900

    increases the timeout of pdf unarchiver test

--- app/test/test_pdf_unarchiver.ts
+++ app/test/test_pdf_unarchiver.ts
@@ -4,6 +4,7 @@ import ImageUtil = require('utils/image');
 var assert = chai.assert;
 
 describe('PdfImageUnarchiver', function () {
+  this.timeout(5000);
   var compressTypes = [
     'None',
     'BZip',
