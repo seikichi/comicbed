015f1e739aa7d70ee2637d57dd98990be7dd5b8c
seikichi <seikichi@kmc.gr.jp>
2013-10-12 06:21:39 +0900

    adds Setting.Setting

--- app/models/setting.ts
+++ app/models/setting.ts
@@ -1,6 +1,7 @@
 import _ = require('underscore');
 import Backbone = require('backbone');
 
+import Page = require('models/page');
 import Screen = require('models/screen');
 import Scaler = require('models/scaler');
 import Unarchiver = require('models/unarchiver');
@@ -12,38 +13,49 @@ module Setting {
     screenSetting(): Screen.Setting;
     unarchiverSetting(): Unarchiver.Setting;
     scalerSetting(): Scaler.Setting;
-    // cacheSetting(): CacheSetting;
-    // inputSetting(): InputSetting;
-    // uiSetting(): UISetting;
   }
-  // export interface CacheSetting {}
-  // export interface InputSetting {}
-  // export interface UISetting {}
 
   export function createFromQueryString(queryString: string): Setting {
-    return undefined;
+    return new SettingImpl(queryString);
   }
 }
-  // export function create(options: {[key:string]:string;} = {}): ModelInterface {
-  //   var attributes: Attributes =  {};
-  //   if ('viewMode' in options && options['viewMode'] in ViewMode) {
-  //     attributes.viewMode = ViewMode[options['viewMode']];
-  //   }
-  //   if ('pageDirection' in options && options['pageDirection'] in PageDirection) {
-  //     attributes.pageDirection = PageDirection[options['pageDirection']];
-  //   }
-  //   if ('page' in options && !_.isNaN(parseInt(options['page'], 10))) {
-  //     attributes.page = parseInt(options['page'], 10);
-  //   }
-  //   if ('canvasScale' in options && !_.isNaN(parseFloat(options['canvasScale']))) {
-  //     attributes.canvasScale = parseInt(options['canvasScale'], 10);
-  //   }
-  //   if ('detectsSpreadPage' in options) {
-  //     attributes.detectsSpreadPage = true;
-  //   }
-  //   if ('displaysOnlyImageInPdf' in options) {
-  //     attributes.displaysOnlyImageInPdf = true;
-  //   }
-  //   logger.info('SettingModel is created: attributes:', JSON.stringify(attributes));
-  //   return new SettingModel(attributes);
-  // }
+
+class ScreenSettingModel extends Backbone.Model implements Screen.Setting {
+  defaults() {
+    return {
+      detectsSpreadPage: true,
+      viewMode: Screen.ViewMode.TwoPage,
+      pageDirection: Screen.PageDirection.R2L,
+    };
+  }
+  detectsSpreadPage() { return <boolean>this.get('detectsSpreadPage'); }
+  viewMode() { return <Screen.ViewMode>this.get('viewMode'); }
+  pageDirection() { return <Screen.PageDirection>this.get('pageDirection'); }
+  isSpreadPage(content: Page.Content): boolean {
+    return content.width > content.height;
+  }
+}
+
+class ScalerSettingModel extends Backbone.Model implements Scaler.Setting {
+  scaleMode() { return Scaler.ScaleMode.AlignVertical; }
+}
+
+class UnarchiverSettingModel extends Backbone.Model implements Unarchiver.Setting {
+  loadImageXObjectOnlyInPdf() { return true; }
+}
+
+class SettingImpl implements Setting.Setting {
+  private _screenSetting: Screen.Setting;
+  private _unarchiverSetting: Unarchiver.Setting;
+  private _scalerSetting: Scaler.Setting;
+
+  constructor(queryString: string) {
+    this._screenSetting = new ScreenSettingModel();
+    this._scalerSetting = new ScalerSettingModel();
+    this._unarchiverSetting = new UnarchiverSettingModel();
+  }
+
+  screenSetting() { return this._screenSetting; }
+  unarchiverSetting() { return this._unarchiverSetting; }
+  scalerSetting() { return this._scalerSetting; }
+}
