ef2bdb871b9343ac81cde333fb14bc5dc312457d
seikichi <seikichi@kmc.gr.jp>
2013-10-17 10:57:26 +0900

    adds strings.startsWith utility

--- app/utils/strings.ts
+++ app/utils/strings.ts
@@ -1,6 +1,10 @@
 export = strings;
 
 module strings {
+  export function startsWith(str: string, prefix: string): boolean {
+    return str.indexOf(prefix) == 0;
+  };
+
   export function endsWith(str: string, suffix: string): boolean {
     return str.indexOf(suffix, str.length - suffix.length) !== -1;
   }
