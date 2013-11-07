be3ccd9a7dffecae6d0bcedc08a3b73d2c5f0803
seikichi <seikichi@kmc.gr.jp>
2013-11-07 13:35:18 +0900

    builds pdf.js modules in setup.sh script and removes the js files from this repository

--- setup.sh
+++ setup.sh
@@ -5,3 +5,10 @@ npm install
 grunt setup
 
 tsd install requirejs underscore
+
+git submodule update --init
+node submodules/pdf.js/make.js generic
+
+mkdir -p assets/app/pdfjs/js/
+cp submodules/pdf.js/build/generic/build/pdf.*.js assets/app/pdfjs/js/
+cp submodules/pdf.js/build/generic/web/compatibility.js assets/app/pdfjs/js/
