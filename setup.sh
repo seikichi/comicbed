9a949c30c074b779aeb50e8366391dd1a6067ff3
seikichi <seikichi@kmc.gr.jp>
2013-11-08 10:09:50 +0900

    adds tiff.js and unrar.js submodules to "submodules" directory

--- setup.sh
+++ setup.sh
@@ -12,3 +12,9 @@ node submodules/pdf.js/make.js generic
 mkdir -p assets/app/pdfjs/js/
 cp submodules/pdf.js/build/generic/build/*.js assets/app/pdfjs/js/
 cp submodules/pdf.js/build/generic/web/compatibility.js assets/app/pdfjs/js/
+
+mkdir -p assets/app/tiff/js/
+cp submodules/tiff.js/tiff.memory_growth.min.js  assets/app/tiff/js/tiff.min.js
+
+mkdir -p assets/app/unrarlib/js/
+cp submodules/unrar.js/unrar.min.js  assets/app/unrarlib/js/unrar.min.js
