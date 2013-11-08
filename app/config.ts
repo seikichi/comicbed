be487e054190602b28dfd1143a75ae0183dccd15
seikichi <seikichi@kmc.gr.jp>
2013-11-08 10:25:38 +0900

    removes the jquery-mobile files from this repository (downloads in setup.sh)

--- app/config.ts
+++ app/config.ts
@@ -17,7 +17,7 @@ require.config({
     pdfjs_compatibility: '../assets/app/pdfjs/js/compatibility',
     unrar: '../assets/app/unrarlib/js/unrar.min',
     tiff: '../assets/app/tiff/js/tiff.min',
-    jquerymobile: '../assets/app/jquery-mobile/js/jquery.mobile-1.4.0-rc.1.min',
+    jquerymobile: '../assets/app/jquery-mobile/js/jquery.mobile-1.4.0-rc.1',
 
     dropbox: 'https://www.dropbox.com/static/api/1/dropins',
     gapi: 'https://apis.google.com/js/api',
--- setup.sh
+++ setup.sh
@@ -18,3 +18,12 @@ cp submodules/tiff.js/tiff.memory_growth.min.js  assets/app/tiff/js/tiff.min.js
 
 mkdir -p assets/app/unrarlib/js/
 cp submodules/unrar.js/unrar.min.js  assets/app/unrarlib/js/unrar.min.js
+
+mkdir -p assets/app/jquery-mobile/js/ assets/app/jquery-mobile/css/  assets/app/jquery-mobile/tmp
+wget http://jquerymobile.com/resources/download/jquery.mobile-1.4.0-rc.1.zip -P assets/app/jquery-mobile
+unzip assets/app/jquery-mobile/jquery.mobile-1.4.0-rc.1.zip -d assets/app/jquery-mobile/tmp
+mv assets/app/jquery-mobile/tmp/jquery.mobile-1.4.0-rc.1.js assets/app/jquery-mobile/js
+mv assets/app/jquery-mobile/tmp/jquery.mobile-1.4.0-rc.1.min.css assets/app/jquery-mobile/css
+mv assets/app/jquery-mobile/tmp/images assets/app/jquery-mobile/css/
+rm assets/app/jquery-mobile/jquery.mobile-1.4.0-rc.1.zip
+rm -rf assets/app/jquery-mobile/tmp
